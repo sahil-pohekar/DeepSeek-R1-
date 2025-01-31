4487e5c1c3fcd5cd7b46b66ce4d3b63efe395dfc
sahil-pohekar <147643106+sahil-pohekar@users.noreply.github.com>
2025-01-31 16:40:30 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+
+about_page = st.Page(
+    page = "views/about.py",
+    title = "Home",
+    icon = ":material/account_circle:",
+    # default = True
+    
+)
+
+project_page = st.Page(
+    page = "views/chatbot.py",
+    title = "DeepThink(R1)",
+    icon = ":material/chat_bubble_outline:"
+)
+
+
+
+pg = st.navigation(
+    {
+        "info" : [about_page],
+        "chat" : [project_page],
+    }
+)
+
+
+st.logo(r"E:\Streamlit\assets\logo.png")
+
+
+pg.run()
